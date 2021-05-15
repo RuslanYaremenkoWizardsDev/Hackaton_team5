2bec83de42e33d90971f2c05b11922d4f05a82ca
tatotschka17@outlook.com <tatotschka17@outlook.com>
2021-05-15 20:34:16 +0300

    added tournament creation form

---
+++ stressProject/stressProject/TournamentCreation.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace stressProject
+{
+    partial class TournamentCreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelNameOfTournament = new System.Windows.Forms.Label();
+            this.labelTournamentDescription = new System.Windows.Forms.Label();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.labelPlace = new System.Windows.Forms.Label();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.labelLastRegistrationDate = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.labelLevelOfTornament = new System.Windows.Forms.Label();
+            this.labelNumberOfParticipants = new System.Windows.Forms.Label();
+            this.comboBoxCountPlayers = new System.Windows.Forms.ComboBox();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.comboBoxLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxScenario = new System.Windows.Forms.ComboBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.labelInvitedPlayers = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(171, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 23);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(171, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 23);
+            this.textBox3.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(534, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(260, 537);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // labelNameOfTournament
+            // 
+            this.labelNameOfTournament.AutoSize = true;
+            this.labelNameOfTournament.Location = new System.Drawing.Point(32, 36);
+            this.labelNameOfTournament.Name = "labelNameOfTournament";
+            this.labelNameOfTournament.Size = new System.Drawing.Size(119, 15);
+            this.labelNameOfTournament.TabIndex = 2;
+            this.labelNameOfTournament.Text = "Name of tournament";
+            // 
+            // labelTournamentDescription
+            // 
+            this.labelTournamentDescription.AutoSize = true;
+            this.labelTournamentDescription.Location = new System.Drawing.Point(32, 73);
+            this.labelTournamentDescription.Name = "labelTournamentDescription";
+            this.labelTournamentDescription.Size = new System.Drawing.Size(133, 15);
+            this.labelTournamentDescription.TabIndex = 3;
+            this.labelTournamentDescription.Text = "Tournament description";
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.Location = new System.Drawing.Point(32, 116);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(38, 15);
+            this.labelMode.TabIndex = 4;
+            this.labelMode.Text = "Mode";
+            // 
+            // labelPlace
+            // 
+            this.labelPlace.AutoSize = true;
+            this.labelPlace.Location = new System.Drawing.Point(32, 162);
+            this.labelPlace.Name = "labelPlace";
+            this.labelPlace.Size = new System.Drawing.Size(84, 15);
+            this.labelPlace.TabIndex = 7;
+            this.labelPlace.Text = "Name of place";
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Location = new System.Drawing.Point(32, 205);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(57, 15);
+            this.labelStartDate.TabIndex = 8;
+            this.labelStartDate.Text = "Start date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(171, 197);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // labelLastRegistrationDate
+            // 
+            this.labelLastRegistrationDate.AutoSize = true;
+            this.labelLastRegistrationDate.Location = new System.Drawing.Point(32, 244);
+            this.labelLastRegistrationDate.Name = "labelLastRegistrationDate";
+            this.labelLastRegistrationDate.Size = new System.Drawing.Size(117, 15);
+            this.labelLastRegistrationDate.TabIndex = 8;
+            this.labelLastRegistrationDate.Text = "Last registration date";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(171, 236);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // labelLevelOfTornament
+            // 
+            this.labelLevelOfTornament.AutoSize = true;
+            this.labelLevelOfTornament.Location = new System.Drawing.Point(32, 280);
+            this.labelLevelOfTornament.Name = "labelLevelOfTornament";
+            this.labelLevelOfTornament.Size = new System.Drawing.Size(114, 15);
+            this.labelLevelOfTornament.TabIndex = 10;
+            this.labelLevelOfTornament.Text = "Level of tournament";
+            // 
+            // labelNumberOfParticipants
+            // 
+            this.labelNumberOfParticipants.AutoSize = true;
+            this.labelNumberOfParticipants.Location = new System.Drawing.Point(32, 321);
+            this.labelNumberOfParticipants.Name = "labelNumberOfParticipants";
+            this.labelNumberOfParticipants.Size = new System.Drawing.Size(130, 15);
+            this.labelNumberOfParticipants.TabIndex = 13;
+            this.labelNumberOfParticipants.Text = "Number of participants";
+            // 
+            // comboBoxCountPlayers
+            // 
+            this.comboBoxCountPlayers.FormattingEnabled = true;
+            this.comboBoxCountPlayers.Items.AddRange(new object[] {
+            "4",
+            "8",
+            "16",
+            "32",
+            "64",
+            "128"});
+            this.comboBoxCountPlayers.Location = new System.Drawing.Point(171, 313);
+            this.comboBoxCountPlayers.Name = "comboBoxCountPlayers";
+            this.comboBoxCountPlayers.Size = new System.Drawing.Size(134, 23);
+            this.comboBoxCountPlayers.TabIndex = 14;
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "Cup",
+            "Championship"});
+            this.comboBoxMode.Location = new System.Drawing.Point(171, 116);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(134, 23);
+            this.comboBoxMode.TabIndex = 15;
+            // 
+            // comboBoxLevel
+            // 
+            this.comboBoxLevel.FormattingEnabled = true;
+            this.comboBoxLevel.Items.AddRange(new object[] {
+            "Begginer",
+            "Middle",
+            "Advanced"});
+            this.comboBoxLevel.Location = new System.Drawing.Point(171, 272);
+            this.comboBoxLevel.Name = "comboBoxLevel";
+            this.comboBoxLevel.Size = new System.Drawing.Size(134, 23);
+            this.comboBoxLevel.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 15);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Scenario for tournament";
+            // 
+            // comboBoxScenario
+            // 
+            this.comboBoxScenario.FormattingEnabled = true;
+            this.comboBoxScenario.Items.AddRange(new object[] {
+            "one-match confrotation",
+            "two-match confrotation",
+            "to three wins"});
+            this.comboBoxScenario.Location = new System.Drawing.Point(174, 355);
+            this.comboBoxScenario.Name = "comboBoxScenario";
+            this.comboBoxScenario.Size = new System.Drawing.Size(131, 23);
+            this.comboBoxScenario.TabIndex = 18;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(188, 585);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(117, 23);
+            this.buttonSave.TabIndex = 19;
+            this.buttonSave.Text = "Create Cup grid";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(174, 412);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(147, 152);
+            this.dataGridView2.TabIndex = 20;
+            // 
+            // labelInvitedPlayers
+            // 
+            this.labelInvitedPlayers.AutoSize = true;
+            this.labelInvitedPlayers.Location = new System.Drawing.Point(32, 412);
+            this.labelInvitedPlayers.Name = "labelInvitedPlayers";
+            this.labelInvitedPlayers.Size = new System.Drawing.Size(83, 15);
+            this.labelInvitedPlayers.TabIndex = 21;
+            this.labelInvitedPlayers.Text = "Invited players";
+            // 
+            // TournamentCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.labelInvitedPlayers);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.comboBoxScenario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxLevel);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.comboBoxCountPlayers);
+            this.Controls.Add(this.labelNumberOfParticipants);
+            this.Controls.Add(this.labelLevelOfTornament);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.labelLastRegistrationDate);
+            this.Controls.Add(this.labelStartDate);
+            this.Controls.Add(this.labelPlace);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.labelTournamentDescription);
+            this.Controls.Add(this.labelNameOfTournament);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "TournamentCreation";
+            this.Text = "TournamentCreation";
+            this.Load += new System.EventHandler(this.TournamentCreation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelNameOfTournament;
+        private System.Windows.Forms.Label labelTournamentDescription;
+        private System.Windows.Forms.Label labelMode;
+        private System.Windows.Forms.Label labelPlace;
+        private System.Windows.Forms.Label labelStartDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label labelLastRegistrationDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label labelLevelOfTornament;
+        private System.Windows.Forms.Label labelNumberOfParticipants;
+        private System.Windows.Forms.ComboBox comboBoxCountPlayers;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.ComboBox comboBoxLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxScenario;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label labelInvitedPlayers;
+    }
+}
