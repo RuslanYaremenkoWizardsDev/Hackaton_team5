ac98cf7e9285b18a3ac46127e4ea93bedac860f6
RockTF <egorov2001egorr@gmail.com>
2021-05-15 12:58:03 +0300

    add new Form her name Archive

--- stressProject/stressProject/AdminPanel.Designer.cs
+++ stressProject/stressProject/AdminPanel.Designer.cs
@@ -31,13 +31,13 @@ namespace stressProject
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // AdminPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 490);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "AdminPanel";
+            this.Text = "AdminForm";
             this.ResumeLayout(false);
 
         }
